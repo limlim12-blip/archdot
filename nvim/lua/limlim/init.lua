f40a8e53d65ae7c5da03265edcb57bb514a6a215
limlim12-blip <lampham15009@gmail.com>
2026-04-02 00:29:12 +0700

    update

--- nvim/lua/limlim/init.lua
+++ nvim/lua/limlim/init.lua
@@ -2,6 +2,10 @@ require("limlim.set")
 require("limlim.remap")
 require("limlim.lazy_init")
 print("helllo from lim")
+if vim.env.TERM == 'xterm-kitty' then
+    vim.cmd([[autocmd UIEnter * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[>1u") | endif]])
+    vim.cmd([[autocmd UILeave * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[<1u") | endif]])
+end
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
