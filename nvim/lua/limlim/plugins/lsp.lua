d6f1d75512a67a49fab042199e6ab6c08ff2a267
limlim12-blip <lampham15009@gmail.com>
2026-04-26 20:15:37 +0700

    new

--- nvim/lua/limlim/plugins/lsp.lua
+++ nvim/lua/limlim/plugins/lsp.lua
@@ -66,7 +66,7 @@ return {
                     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
                     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
                     vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-                    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+                    vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, opts)
                     vim.keymap.set("n", "<leader>gn", function() vim.lsp.buf.rename() end, opts)
                     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
